18554bad7ca9376bc4fb6c9e99caf27775307e43
Stefanie Chloe Edgar <chloe.tara@gmail.com>
2012-02-23 13:09:23 -0900

    Now checks for "HTTP 200" response before counting a request as successful. Updated README, fixed some formatting.

--- heavy_load.c
+++ heavy_load.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <time.h>
+#include <string.h>
 
 /* 150 threads running 900 sockets each => 135k connections and having each socket 
 doing 10000 requests should be enough coming from one program instance */
@@ -21,6 +22,7 @@ doing 10000 requests should be enough coming from one program instance */
 #define THREADS 150
 #define REQUESTS 10000
 #define DURATION 30 
+#define READ_BUFFER 1024
 
 /* TODO: 
 - take into account round trip time for one request, serialization speed
@@ -35,55 +37,61 @@ low and increase them if necessary
 long *threads = NULL;
 
 int make_socket(char *host, char *port) {
-	struct addrinfo hints, *servinfo, *p;
-	int sock, r;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
-		exit(0);
-	}
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			continue;
-		}
-		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
-			close(sock);
-			continue;
-		}
-		break;
-	}
-	if(p == NULL) {
-		if(servinfo)
-			freeaddrinfo(servinfo);
-		fprintf(stderr, "No connection could be made\n");
-		exit(0);
-	}
+    struct addrinfo hints, *servinfo, *p;
+    int sock, r;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
+        exit(0);
+    }
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            continue;
+        }
+        if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
+            close(sock);
+            continue;
+        }
+        break;
+    }
+    if(p == NULL) {
+        if(servinfo)
+            freeaddrinfo(servinfo);
+        fprintf(stderr, "No connection could be made\n");
+        exit(0);
+    }
 /* otherwise you got a memleak */
-	freeaddrinfo(servinfo);
-	return sock;
+    freeaddrinfo(servinfo);
+    return sock;
 }
 
 void httpload(char *host, char *port, int count) {
-	/* initialize array & variables */ 
-	int sockets[CONNECTIONS] = { 0 };
-	int x, r, y;
+    /* initialize array & variables */ 
+    int sockets[CONNECTIONS] = { 0 };
+    int x, req, y, response;
+    char server_response[READ_BUFFER]; 
 
     /* http header/s */
     char http_header[31] = "HEAD /test.txt HTTP/1.0\r\n\r\n \0";
 
-	/* ignore SIGPIPE */
-	signal(SIGPIPE, SIG_IGN);
-	while(1) {
-	    for(x = 0; x < CONNECTIONS; x++) {
-	        if(sockets[x] == 0) {
+    /* ignore SIGPIPE */
+    signal(SIGPIPE, SIG_IGN);
+    while(1) {
+        for(x = 0; x < CONNECTIONS; x++) {
+            if(sockets[x] == 0) {
                 sockets[x] = make_socket(host, port);
                 for (y = 0; y < REQUESTS; y++) {
-                    r=send(sockets[x], http_header, 31, MSG_NOSIGNAL);
-                    threads[count] += 1;
+                    req=send(sockets[x], http_header, 31, MSG_NOSIGNAL);
+                    response=read(sockets[x], server_response, READ_BUFFER); 
+ 
+                    /* if the server returns "200", increment number of successful requests */
+                    if (strstr(server_response, "200"))  {
+                       threads[count] += 1; 
+                    }
                 }
-                if(r == -1) {
+                if(req == -1) {
                     close(sockets[x]);
                     sockets[x] = make_socket(host, port);
                     /* CHANGEME: should another for loop be run here ^^ */
@@ -92,14 +100,14 @@ void httpload(char *host, char *port, int count) {
                 }
             }
         }
-		usleep(10000);
-	}
+        usleep(10000);
+    }
 }
 
 int main(int argc, char **argv) {
 
-	/* Setting up shared memory for the 'requests' variable
-	since it needs to be accessed by many forks. */
+    /* Setting up shared memory for the 'requests' variable
+    since it needs to be accessed by many forks. */
     int shr = shmget(IPC_PRIVATE, sizeof(long)*THREADS, 0600);
     threads = (long *) shmat(shr, NULL, 0);
 
@@ -107,50 +115,50 @@ int main(int argc, char **argv) {
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
-	/* CHANGEME: to be or not to be +1 */
-	pid_t offspring[THREADS+1] = { '\0' };
-	int x;
+    /* CHANGEME: to be or not to be +1 */
+    pid_t offspring[THREADS+1] = { '\0' };
+    int x;
         int count;
 
     /* create a new thread for each httpload() instance */
-	for(x = 0; x < THREADS; x++) {
-		pid_t pid = fork();
-		if(0 == pid) { 							/* child process */
-			offspring[x] = getpid();
-			httpload(argv[1], argv[2], x);
-			usleep(10000);						/* 10 ms delay between spawning processes */
-		}
-		if(pid < 0) { 							/* failed to fork */
-			fprintf(stderr, "Can't fork\n");
-			exit(1);
-		}
+    for(x = 0; x < THREADS; x++) {
+        pid_t pid = fork();
+        if(0 == pid) {                             /* child process */
+            offspring[x] = getpid();
+            httpload(argv[1], argv[2], x);
+            usleep(10000);                        /* 10 ms delay between spawning processes */
+        }
+        if(pid < 0) {                             /* failed to fork */
+            fprintf(stderr, "Can't fork\n");
+            exit(1);
+        }
     }
 
-	/* print number of requests every second */
+    /* print number of requests every second */
     /* generate load for the DURATION of the test */
     struct timeval now;
     for (count=0; count < DURATION ; count++) {
-		int i;
-	    long long requests = 0;
+        int i;
+        long long requests = 0;
     for(i = 0; i < THREADS; i++) {
         gettimeofday(&now, NULL);
         printf("%ld\n", requests);
-        printf("program starttime: %ld\n", start.tv_sec);
-        printf("thread nowtime: %ld\n", now.tv_sec);
+        //printf("program starttime: %ld\n", start.tv_sec); // debug
+        //printf("thread nowtime: %ld\n", now.tv_sec);      // debug
 
         requests += threads[i];
         printf("requests made total: %lld\naverage requests/second: %lld\n", requests, (long long)requests/(((now.tv_sec) - (start.tv_sec)) +1)); 
         sleep(1); 
-		}
-	}
+        }
+    }
 
     getc(stdin);
 
-	/* kill child pids */
-	int pids;
+    /* kill child pids */
+    int pids;
 
-	for(pids = 0; pids < THREADS; pids++) {
-		kill(offspring[pids],SIGKILL);
-	}
-	return 0;
+    for(pids = 0; pids < THREADS; pids++) {
+        kill(offspring[pids],SIGKILL);
+    }
+    return 0;
 }
